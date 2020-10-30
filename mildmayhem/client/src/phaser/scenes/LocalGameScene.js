0d227cfad4039a34160350108b695a6d3bb73689
Wrightj9 <Wrightj9@wit.edu>
2020-10-30 12:01:17 -0400

    Fixed Data Passing between react and phaser3
    The groundwork has been set to be able to pass data from React into Phaser3, such as the controller configuration.

--- mildmayhem/client/src/phaser/scenes/LocalGameScene.js
+++ mildmayhem/client/src/phaser/scenes/LocalGameScene.js
@@ -5,13 +5,14 @@ import MagicBlast from "../sprites/MagicBlast.js";
 import SwordSwing from "../sprites/SwordSlash.js";
 import Phaser from 'phaser';
 import bckg from '../assets/bckg.png';
-class LocalGameScene extends Scene {
-    preload ()
+let LocalGameScene = {
+    
+    
+    preload: function()
         {
             this.load.image('bckg', 'https://i.imgur.com/DMVC2IQ.png');
             this.load.image('wall', 'https://i.imgur.com/av8q7Or.png');
             this.load.image('vwall', 'https://i.imgur.com/UB2GdEL.png');
-            //this.load.image('sword', '../assets/Sword.png');
             this.load.spritesheet('swordSwing', 'https://i.imgur.com/ULyXfap.png', {frameWidth: 500, frameHeight: 500});
             this.load.image('swordCool', 'https://i.imgur.com/SnLjQdR.png');
             this.load.image('blastCool', 'https://i.imgur.com/d4Enueh.png');
@@ -19,41 +20,49 @@ class LocalGameScene extends Scene {
             this.load.spritesheet('explosion','https://i.imgur.com/UHZiUKC.png', {frameWidth: 192, frameHeight: 192});
             this.load.spritesheet('player', 'https://i.imgur.com/nRFZx7v.png', { frameWidth: 68, frameHeight: 68 });
             this.load.spritesheet('otherPlayer','https://i.imgur.com/V78wgrC.png', { frameWidth: 68, frameHeight: 68 });
-        }
+        },
 
-    create ()
+    create: function()
         {
-            this.player = new Player(this,250,250,'player');
-            this.controls = new Controls(this,{directionals: 'WASD', magicBlast: 'p', swordSwing: 'SPACE'});
+            this.deflectBlast = function(magicBlast,swordHitbox){
+                if(swordHitbox.inSwordSweep(magicBlast)){
+                    magicBlast.deflectFrom(swordHitbox.getOwner());
+                }
+
+            };
+            console.log("config is: " + this.controlConfig);
+            this.player1 = new Player(this, 400, 500,'player');
+            this.player2 = new Player(this, 400, 100, 'otherPlayer');
+            this.controlsP1 = new Controls(this,{directionals: 'WASD', magicBlast: 'p', swordSwing: 'SPACE'});
+            this.controlsP2 = new Controls(this,{directionals: 'WASD', magicBlast: 'p', swordSwing: 'SPACE'});
             this.magicBlasts = this.physics.add.group();
             this.swordHitBoxes = this.physics.add.group();
             this.physics.add.overlap(this.magicBlasts,this.swordHitBoxes,this.deflectBlast);
-            //this.physics.add.collider(this)
-        
-            //this.magicBlastGroup.createMagicBlast(this,300,300,'magicBlast');
-            ///this.magicBlasts.create(300,300,'magicBlast');
-        }
+            
+        },
 
-    update ()
+    update: function()
         {
+        //console.log("control config is: " + this.controlConfig);
         //get Player input
-        this.movementVector = this.controls.getMovementVector();
+        this.movementVector = this.controlsP1.getMovementVector();
+        this.movementVector = this.controlsP2.getMovementVector();
+
+        this.player1.setPlayerVelocity(this.movementVector);
+        this.player1.setOrientationVector(this.movementVector);
+        let attackInputs = this.controlsP1.getAttackInput();
         
-        this.player.setPlayerVelocity(this.movementVector);
-        this.player.setOrientationVector(this.movementVector);
-        let attackInputs = this.controls.getAttackInput();
-        
-        //this.physics.add.overlap(this.player, this.magicBlasts,this.deflectBlast,this);
+        //this.physics.add.overlap(this.player1, this.magicBlasts,this.deflectBlast,this);
         //Check for user firing magic blast
         if (attackInputs.magicBlastFiring){
             //Create magic Blast
-            var magicBlast = new MagicBlast(this,this.player.getX(),
-            this.player.getY(),'magicBlast');
+            var magicBlast = new MagicBlast(this,this.player1.getX(),
+            this.player1.getY(),'magicBlast');
             
             //Add to collision group
             this.magicBlasts.add(magicBlast);
             //Fire in direction of player orientation
-            magicBlast.setMagicBlastVelocity(this.player.getOrientationVector());
+            magicBlast.setMagicBlastVelocity(this.player1.getOrientationVector());
             
             magicBlast.setCollideWorldBounds(true);
             magicBlast.setBounce(1);
@@ -68,12 +77,12 @@ class LocalGameScene extends Scene {
         if ((attackInputs.swordSwingFiring && typeof this.swordSwing == "undefined") || attackInputs.swordSwingFiring && !this.swordSwing.isSwinging()){
             //console.log("Sword Swung!!");
             //Set sword swing spawn point
-            let swordSpawnX = this.player.getX();
-            let swordSpawnY = this.player.getY();
+            let swordSpawnX = this.player1.getX();
+            let swordSpawnY = this.player1.getY();
             //console.log("swordSpawnX is:" + swordSpawnX);
             //console.log("swordSpawnY is:" + swordSpawnY);
             //Create new sword swing
-            this.swordSwing = new SwordSwing(this,swordSpawnX,swordSpawnY,'swordSwing',{owner: this.player});
+            this.swordSwing = new SwordSwing(this,swordSpawnX,swordSpawnY,'swordSwing',{owner: this.player1});
             this.swordSwing.swingSword();
             this.swordHitBoxes.add(this.swordSwing);
             
@@ -81,12 +90,8 @@ class LocalGameScene extends Scene {
 
         
         }
-    deflectBlast(magicBlast,swordHitbox){
-            if(swordHitbox.inSwordSweep(magicBlast)){
-                magicBlast.deflectFrom(swordHitbox.getOwner());
-            }
-
-        }
+    
+    
 
 
 }
