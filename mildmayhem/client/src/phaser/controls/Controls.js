95bdf543c818e1c395d637fd50a058f2bead340a
Wrightj9 <Wrightj9@wit.edu>
2020-11-02 15:34:10 -0500

    Updated Controls JS
    GamePad movement setup works now.

--- mildmayhem/client/src/phaser/controls/Controls.js
+++ mildmayhem/client/src/phaser/controls/Controls.js
@@ -7,6 +7,7 @@ export default class Controls extends Phaser.GameObjects.Sprite
             super(scene,type);
             this.setKeyInput(scene,type);
             this.scene = scene;
+            
         }
         setKeyInput(scene,{directionals,magicBlast,swordSwing,dodge}={}){
           //Potentially can add more configurable controls by using if statements here in the future
@@ -23,9 +24,8 @@ export default class Controls extends Phaser.GameObjects.Sprite
            
           }
           else if (directionals === 'GamePad'){
-            
-            //Checks to see if a gamepad exists
-            let timedEvent = this.scene.time.delayedCall(100, this.onEvent, [], this);
+            this.gamePadMode = true;
+            console.log("game pad mode active" + this.gamePadMode);
             const xAxis = 0;
             const yAxis = 0;
             this.directionals = {
@@ -34,6 +34,20 @@ export default class Controls extends Phaser.GameObjects.Sprite
                 left: {isDown: xAxis > 0 ? true : false},
                 right: {isDown: xAxis > 0 ? true : false},
               }
+            /*
+            this.scene.input.gamepad.on('down', function (pad, button, index) {
+               console.log("connected!");
+                
+                this.scene.input.gamepad = pad;
+                this.directionals = {
+                  up: {isDown: this.scene.input.gamepad.axes[0].getValue() > 0 ? true : false},
+                  down: {isDown: this.scene.input.gamepad.axes[0].getValue() < 0 ? true : false},
+                  left: {isDown: this.scene.input.gamepad.axes[0].getValue() > 0 ? true : false},
+                  right: {isDown: this.scene.input.gamepad.axes[0].getValue() > 0 ? true : false},
+              }
+                
+            }, this);
+           */
           }
           if (magicBlast === 'p'){
             this.magicBlastAttack = scene.input.keyboard.addKeys({p: Phaser.Input.Keyboard.KeyCodes.P});
@@ -48,23 +62,40 @@ export default class Controls extends Phaser.GameObjects.Sprite
             this.swordSwing = scene.input.keyboard.addKeys({spaceBar: Phaser.Input.Keyboard.KeyCodes.NUMPAD_ZERO});
           }
         }
-        onEvent(){
-          console.log("Event has been called");
-           if (this.scene.input.gamepad.total){
-                console.log("gamePad detected!");
-                this.pad = this.scene.input.gamepad.getPad(0);
-                let xAxis = this.pad.axes[0].getValue();
-                let yAxis = this.pad.axes[1].getValue();
-                /*this.directionals = {
+        
+        getMovementVector(){
+         
+          if (this.gamePadMode){
+
+            let pad: Phaser.Input.Gamepad.Gamepad;
+            if (this.scene.input.gamepad.total){ 
+                pad = this.scene.input.gamepad.getPad(0);
+
+                const xAxis = pad.axes[0].getValue();
+                const yAxis = pad.axes[1].getValue();
+               
+                this.directionals = {
                   up: {isDown: yAxis < 0 ? true : false},
                   down: {isDown: yAxis > 0 ? true : false},
-                  left: {isDown: xAxis > 0 ? true : false},
+                  left: {isDown: xAxis < 0 ? true : false},
                   right: {isDown: xAxis > 0 ? true : false},
-                }*/
-                
+              }
             }
-        }
-        getMovementVector(){
+
+
+            /*
+            if(this.input.gamepad.total){
+              let pad = this.scene.input.gamepad.getPad(0);
+              console.log("we have set up the gamepad thing");
+               this.directionals = {
+                  up: {isDown: pad.axes[0].getValue() > 0 ? true : false},
+                  down: {isDown: pad.gamepad.axes[0].getValue() < 0 ? true : false},
+                  left: {isDown: pad.axes[0].getValue() > 0 ? true : false},
+                  right: {isDown: pad.axes[0].getValue() > 0 ? true : false},
+              }
+            }
+            */
+           }
          return {
            //Gets the vector for a directional press(y is inverted)
            x: ((this.directionals.right.isDown)? 1:0)+((this.directionals.left.isDown)? -1:0),
