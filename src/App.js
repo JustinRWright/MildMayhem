7e4fdd8d08e6fe5f2f119f40503b2734d72f442d
JustinRWright <JustinR.WrightEng@gmail.com>
2020-11-13 00:16:54 -0500

    Shoved the game in here
    I moved the build code for the react game into the deployed branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>adfasdfasdfasdf.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import styles from './styles/main.css';
+import MenuPage from './pages/MenuPage.js';
+import GamePage from './pages/GamePage.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Phaser from 'phaser'
+import { IonPhaser } from '@ion-phaser/react';
+class App extends Component {
+ constructor(props) {
+            super(props);
+            //ControlConfig is a default value
+            this.state = {controlConfig: {
+                player1: {
+                    Movement: 'WASD',
+                    SwordSlash: 'SPACE',
+                    MagicBlast: 'P'
+                },
+                player2: {
+                    Movement: 'ArrowKeys',
+                    SwordSlash: 'NumPad0',
+                    MagicBlast: 'NumPad9'
+                }
+            }
+          }
+          };
+  controlConfig = (dataFromMenuPage) => {
+    console.log("controls data from MenuPage is: " + dataFromMenuPage);
+    this.setState({controlConfig: dataFromMenuPage});
+    console.log("state is now: " + JSON.stringify(this.state.controlConfig));
+  };
+ 
+  render() {
+    
+    return (
+      
+      <Router>
+        <div>
+            <Switch>
+              <Route path="/" exact >
+                <MenuPage passControlConfig={this.controlConfig}/>
+              </Route>
+              <Route path="/game">
+                <GamePage controlConfig={this.state.controlConfig}/>
+              </Route>
+            </Switch>
+         
+        </div>
+      </Router>
+      
+    )
+  }
 }
 
 export default App;
