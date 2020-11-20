62df8b0132cb6a2eb6242c75be7283fda12de5f8
JustinRWright <JustinR.WrightEng@gmail.com>
2020-11-20 13:10:09 -0500

    Added readme and wireframe diagrams

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
             }
           }
           };
-  //callback function which comes all the way from the button component in the React application, without redux, 
+  //Callback function which comes all the way from the button component in the React application, without redux, 
   //I believe state has to be managed/updated like this, which I think it isn't the best way to do it
   //I forget the term but basically you have to pass this one callback function through the entire program if there
   // isn't a common parent component
