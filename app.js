91f14319f1cba29212e6cdddd561acb62d642420
JustinRWright <JustinR.WrightEng@gmail.com>
2020-11-30 21:16:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header(
     "Access-Control-Allow-Headers",
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+*/
 var gameRooms = {};
 var roomCount = 0;
 var players = {};
