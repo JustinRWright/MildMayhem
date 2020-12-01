1cbdb8eab0974046370763b00d29802a4e43f5f8
JustinRWright <JustinR.WrightEng@gmail.com>
2020-11-30 21:11:07 -0500

    Working verion of game

--- app.js
+++ app.js
@@ -4,36 +4,33 @@ const path = require('path');
 //const socketIo = require("socket.io");
 
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 const server = require('http').Server(app);
 const io = require("socket.io")(server, {
-<<<<<<< HEAD
   path: '/socket'
-=======
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["content-type"],
-  }
->>>>>>> parent of 6c8a4aa7... Update app.js
+});
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public'));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 // Add Access Control Allow Origin headers
-/*app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-});*/
+});
 var gameRooms = {};
 var roomCount = 0;
 var players = {};
