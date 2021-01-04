e21c7098730eab26faf59b9a9d59b0f3f65694e0
JustinRWright <JustinR.WrightEng@gmail.com>
2021-01-03 23:49:11 -0500

    Sword marker render on selection

--- client/src/components/menuTabButton.js
+++ client/src/components/menuTabButton.js
@@ -1,21 +1,31 @@
 import React, { useState,useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import '../styles/main.scss';
-
+import BrickImage from '../images/ImpressionistBrickButtonG.jpg';
+import SwordMarker from '../images/SwordMarker.png';
 const MenuTabButton = (props) => {
     const toggle = () => {
         props.onClick();
     };
         let highlightColor;
+        let swordMarker;
         if (props.selected){
             highlightColor = "#00FFFF";
+            swordMarker = <img style={{backgroundSize: 'cover', height: 30, position: 'absolute', top: 4, left: 10}} src={SwordMarker} alt="this is car image" />;
         }
         else {
             highlightColor = "black";
+            swordMarker = <div></div>
         }
         return <div>
-            <div onClick={toggle} className="menuTabButton" style = {{paddingTop: 7, paddingBottom: 7, textAlign: 'center', margin: 'auto', backgroundColor: 'grey', width: '70%', cursor: 'pointer', borderRadius: 5, border: ("2px solid "+ highlightColor)}}>
+            <div onClick={toggle} className="menuTabButton" style = {{ position: 'relative', textAlign: 'center', margin: 'auto', height: 39.5, backgroundColor: 'grey', width: '70%', cursor: 'pointer', borderRadius: 3, border: ("1px solid "+ highlightColor)}}>
+                <img style={{backgroundSize: 'cover', opacity: .3, position: 'relative', width: '100%', top: 0, left: 0}} src={BrickImage} alt="this is car image" />
+                {swordMarker}
+                <div style={{fontSize: 20, fontFamily: 'Audiowide', position: 'absolute', textAlign: 'center', top: 4, left: 38}}>
+                
                 {props.title}
+                </div>
+                
             </div>
         </div>
     
